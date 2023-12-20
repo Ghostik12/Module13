36c55bcbcf1427f4c9cab5f449113fca4dd0a104
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-12-20 21:04:23 +0300

    13.6.2

--- Module13/Module13/Program.cs
+++ Module13/Module13/Program.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Diagnostics.Tracing;
+using System.Linq;
 using System.Reflection;
 using System.Security.Authentication;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+using String = System.String;
 
 namespace Module13
 {
@@ -17,17 +20,25 @@ namespace Module13
         static void Main(string[] args)
         {
             string text = File.ReadAllText("C:\\Users\\Ghosman\\Desktop\\Text1.txt");
-            
-            var words = text.Split(' ');
-            var stopWatchList = Stopwatch.StartNew();
+            var words = text.Split(new[] { ',', '.', ' ', ';', ':', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
 
-            List<string> listOne = new List<string>(words);
-            Console.WriteLine(stopWatchList.Elapsed.TotalMilliseconds);
+            var listOne = new Dictionary<string, int>();
+            foreach (var word in words)
+                if (listOne.ContainsKey(word))
+                    listOne[word]++;
+                else
+                    listOne.Add(word, 1);
 
-            var stopWatchLinked = Stopwatch.StartNew();
-
-            LinkedList<string> listTwo = new LinkedList<string>(words);
-            Console.WriteLine(stopWatchLinked.Elapsed.TotalMilliseconds);
+            var sortDict = listOne.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            var i = 0;
+            while (true) 
+            {
+                if (i == 10)
+                    break;
+                Console.WriteLine(sortDict.Last());
+                    sortDict.Remove(sortDict.Keys.Last());
+                    i++;
+            }
         }
     }
 }
