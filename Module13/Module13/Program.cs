da64ee51fea8a912d0df830165f1a217e22a456b
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-12-20 13:46:21 +0300

    13.6.1

--- Module13/Module13/Program.cs
+++ Module13/Module13/Program.cs
@@ -1,45 +1,33 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Diagnostics.Tracing;
 using System.Reflection;
 using System.Security.Authentication;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Module13
 {
     class Program
     {
-        private static Stack<string> PhoneBook = new Stack<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в стек или Введите pop если хотите удалить полседнее слово, или peek если хотите увидеть последнее слово или пропустите.");
-            while (true)
-            {
-                var input = Console.ReadLine();
-                switch (input)
-                {
-                    case "pop":
-                        PhoneBook.TryPop(out string popResult);
-                        break;
-                    case "peek":
-                        PhoneBook.TryPeek(out string peekResult);
-                        break;
-                    default:
-                        PhoneBook.Push(input);
-                        break;
-                }
+            string text = File.ReadAllText("C:\\Users\\Ghosman\\Desktop\\Text1.txt");
+            
+            var words = text.Split(' ');
+            var stopWatchList = Stopwatch.StartNew();
 
-                //Console.WriteLine();
-                //    Console.WriteLine("В стеке: ");
+            List<string> listOne = new List<string>(words);
+            Console.WriteLine(stopWatchList.Elapsed.TotalMilliseconds);
 
-                //    foreach (var word in PhoneBook)
-                //    {
-                //        Console.WriteLine(word);
-                //    }
-            }
+            var stopWatchLinked = Stopwatch.StartNew();
+
+            LinkedList<string> listTwo = new LinkedList<string>(words);
+            Console.WriteLine(stopWatchLinked.Elapsed.TotalMilliseconds);
         }
     }
 }
